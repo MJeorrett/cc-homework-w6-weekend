b564a335e9c6aa8725293c1f6d1b5f51b69da0e3
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-04 22:16:33 +0000

    moving player name input handling to CardGame class

--- blackjack/Blackjack.java
+++ blackjack/Blackjack.java
@@ -11,39 +11,17 @@ public class Blackjack {
   private CardGame cardGame;
 
   public Blackjack() {
-    String[] playerNames = this.getPlayerNames();
+    
+    String[] playerNames = CardGame.getNames( 0, MAX_NUMBER_PLAYERS );
     this.cardGame = new CardGame( playerNames );
-  }
-
-  private String[] getPlayerNames() {
-    Scanner inputScanner = new Scanner( System.in );
-    System.out.print( String.format( "Enter the number of players:( 1 -> %d ): ", MAX_NUMBER_PLAYERS ) );
-    int numberOfPlayers = inputScanner.nextInt();
-
-    while ( numberOfPlayers < 1 || numberOfPlayers > MAX_NUMBER_PLAYERS ) {
-      System.out.println( "Please enter a number between 0 and 10" );
-      System.out.print( "Number of players: ");
-      numberOfPlayers = inputScanner.nextInt();
-    }
-
-    ArrayList<String> playerNames = new ArrayList<String>();
-
-    for ( int i = 1; i <= numberOfPlayers; i++ ) {
-      System.out.print( String.format( "Enter the name for player %d: ", i ) );
-      String name = inputScanner.next();
-      playerNames.add( name );
-    }
 
     System.out.println( "---" );
     System.out.println( "Ready to start with players: " );
+
     for ( String name : playerNames ) {
       System.out.println( "  " + name );
     }
 
-    String[] result = new String[playerNames.size()];
-    playerNames.toArray( result );
-
-    return result;
   }
 
 }
--- card_game_lib/CardGame.java
+++ card_game_lib/CardGame.java
@@ -40,4 +40,29 @@ public class CardGame {
     player.addCard( this.deck.dealCard() );
   }
 
+  public static String[] getNames( int min, int max ) {
+    Scanner inputScanner = new Scanner( System.in );
+    System.out.print( String.format( "Enter the number of players:( %d -> %d ): ", min, max ) );
+    int numberOfPlayers = inputScanner.nextInt();
+
+    while ( numberOfPlayers < min || numberOfPlayers > max ) {
+      System.out.println( String.format( "Please enter a number between %d and %d", min, max ) );
+      System.out.print( "Number of players: ");
+      numberOfPlayers = inputScanner.nextInt();
+    }
+
+    ArrayList<String> playerNames = new ArrayList<String>();
+
+    for ( int i = 1; i <= numberOfPlayers; i++ ) {
+      System.out.print( String.format( "Enter the name for player %d: ", i ) );
+      String name = inputScanner.next();
+      playerNames.add( name );
+    }
+
+    String[] result = new String[playerNames.size()];
+    playerNames.toArray( result );
+
+    return result;
+  }
+
 }
