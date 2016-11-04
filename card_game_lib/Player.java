9b1558ead10303b9deb95be078bb1df46c63c79d
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-04 20:27:05 +0000

    making Player extend Hand

--- card_game_lib/Player.java
+++ card_game_lib/Player.java
@@ -1,29 +1,15 @@
 package card_game_lib;
 
-public class Player {
+public class Player extends Hand {
 
   private String name;
-  private Hand hand;
 
   public Player( String name ) {
     this.name = name;
-    this.hand = new Hand();
   }
 
   public String getName() {
     return this.name;
-  }
-
-  public void addCard( Card card ) {
-    this.hand.addCard( card );
-  }
-
-  public boolean hasCard( Card card ) {
-    return this.hand.hasCard( card );
-  }
-
-  public void removeCard( Card card ) {
-    this.hand.removeCard( card );
   }
 
 }
