af7d30a63fea165fccc68dbc1f2bc2939e279af4
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-04 20:12:48 +0000

    removing package statement as causes problems with running

--- tests/PlayerTest.java
+++ tests/PlayerTest.java
@@ -1,5 +1,3 @@
-package card_game_lib;
-
 import static org.junit.Assert.assertEquals;
 import org.junit.*;
 
--- tests/Runner.java
+++ tests/Runner.java
@@ -1,5 +1,3 @@
-package card_game_lib;
-
 import card_game_lib.*;
 
 public class Runner {
