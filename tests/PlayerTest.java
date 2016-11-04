5e2c3fd3604d81f08c0a949312d267a685fe0c88
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-04 20:28:25 +0000

    removing tests no longer require as Player now extends hand

--- tests/PlayerTest.java
+++ tests/PlayerTest.java
@@ -6,33 +6,14 @@ import card_game_lib.*;
 public class PlayerTest {
 
   Player testPlayer;
-  Card threeOfSpades;
-  Card fiveOfDiamonds;
 
   @Before
   public void before() {
     this.testPlayer = new Player( "Martin" );
-    this.threeOfSpades = new Card( Suit.SPADES, Rank.THREE );
-    this.fiveOfDiamonds = new Card( Suit.DIAMONDS, Rank.FIVE );
   }
 
   @Test
   public void hasName() {
     assertEquals( "Martin", this.testPlayer.getName() );
-  }
-
-  @Test
-  public void hasCardAfterAdding() {
-    this.testPlayer.addCard( this.threeOfSpades );
-    assertEquals( true, this.testPlayer.hasCard( this.threeOfSpades ) );
-  }
-
-  @Test
-  public void removeCard() {
-    this.testPlayer.addCard( this.threeOfSpades );
-    this.testPlayer.addCard( this.fiveOfDiamonds );
-    this.testPlayer.removeCard( this.threeOfSpades );
-    assertEquals( false, this.testPlayer.hasCard( this.threeOfSpades));
-    assertEquals( true, this.testPlayer.hasCard( this.fiveOfDiamonds));
   }
 }
