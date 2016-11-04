5be4dbb5e4d55c566c410c616c748b09c659366e
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-04 19:25:00 +0000

    adding hasCard and takeCard methods to Player

--- card_game_lib/Player.java
+++ card_game_lib/Player.java
@@ -2,14 +2,24 @@ package card_game_lib;
 
 public class Player {
 
-  String name;
+  private String name;
+  private Hand hand;
 
   public Player( String name ) {
     this.name = name;
+    this.hand = new Hand();
   }
 
   public String getName() {
     return this.name;
+  }
+
+  public void takeCard( Card card ) {
+    this.hand.addCard( card );
+  }
+
+  public boolean hasCard( Card card ) {
+    return this.hand.hasCard( card );
   }
 
 }
--- tests/PlayerTest.java
+++ tests/PlayerTest.java
@@ -6,15 +6,23 @@ import card_game_lib.*;
 public class PlayerTest {
 
   Player testPlayer;
+  Card threeOfSpades;
 
   @Before
   public void before() {
     this.testPlayer = new Player( "Martin" );
+    this.threeOfSpades = new Card( Suit.SPADES, Rank.THREE );
   }
 
   @Test
   public void hasName() {
     assertEquals( "Martin", this.testPlayer.getName() );
+  }
+
+  @Test
+  public void hasCardAfterTaking() {
+    this.testPlayer.takeCard( this.threeOfSpades );
+    assertEquals( true, this.testPlayer.hasCard( this.threeOfSpades ) );
   }
 
 }
